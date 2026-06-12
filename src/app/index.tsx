5b71f39c9f56c883aaefe3a83dbb47d31881cd2b
Angel <angelsol299@hotmail.com>
2026-06-12 19:13:11 +0200

    chore: added fonts and title and sub title to page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,11 +4,37 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { ThemedView } from "@/components/themed-view";
 import { Card } from "@/components/ui/Card";
 import { BottomTabInset, MaxContentWidth, Spacing } from "@/constants/theme";
+import { Design } from "@/namespaces/Design";
+
+import {
+  SplineSansMono_400Regular,
+  SplineSansMono_700Bold,
+  useFonts,
+} from "@expo-google-fonts/spline-sans-mono";
+
+import {
+  SourceSerif4_400Regular,
+  SourceSerif4_600SemiBold,
+} from "@expo-google-fonts/source-serif-4";
+
+import { Inter_400Regular, Inter_600SemiBold } from "@expo-google-fonts/inter";
 
 export default function HomeScreen() {
+  const [fontsLoaded] = useFonts({
+    SplineSansMono_400Regular,
+    SplineSansMono_700Bold, // load only the weights you use
+    SourceSerif4_400Regular,
+    SourceSerif4_600SemiBold,
+    Inter_400Regular, // system-ui equivalent for UI/body text
+    Inter_600SemiBold,
+  });
+
+  if (!fontsLoaded) return null;
   return (
     <ThemedView style={styles.container}>
       <SafeAreaView style={styles.safeArea}>
+        <Text style={styles.subTitle}>Secure Generator</Text>
+        <Text style={styles.title}>Password</Text>
         <Card>
           <Text>hellow world</Text>
         </Card>
@@ -20,16 +46,27 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
     flexDirection: "row",
     backgroundColor: "#f2e9df",
   },
   safeArea: {
     flex: 1,
     paddingHorizontal: Spacing.four,
-    alignItems: "center",
-    gap: Spacing.three,
+    gap: Design.space.medium,
     paddingBottom: BottomTabInset + Spacing.three,
     maxWidth: MaxContentWidth,
+  },
+  subTitle: {
+    fontFamily: "SplineSansMono_400Regular",
+    fontSize: 16,
+    color: Design.color.darkGold,
+    marginTop: 24,
+    letterSpacing: 2,
+  },
+  title: {
+    fontFamily: "Inter_600SemiBold",
+    fontSize: Design.fontSize.xxLarge,
+    color: Design.color.blue,
+    letterSpacing: -2.5,
   },
 });
--- src/namespaces/Design.ts
+++ src/namespaces/Design.ts
@@ -1,5 +1,7 @@
 export namespace Design {
   export const fontSize = {
+    xxLarge: 48,
+    xLarge: 32,
     large: 24,
     medium: 16,
     small: 12,
@@ -8,6 +10,8 @@ export namespace Design {
   export const color = {
     white: "#FFFFFF",
     beige: "#f2e9df",
+    darkGold: "#a2917f",
+    blue: "#5e9bd1",
   };
 
   export const space = {
