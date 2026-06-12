664ee5d55a4d1519831d77fbd4c14588e8ebde2f
Angel <angelsol299@hotmail.com>
2026-06-12 18:37:34 +0200

    chore; removed un-used files and added Design and basic card

---
+++ src/components/ui/Card.tsx
@@ -0,0 +1,16 @@
+import { Design } from "@/namespaces/Design";
+import { StyleSheet, View } from "react-native";
+
+export const Card = ({ children }) => {
+  return <View style={styles.container}>{children}</View>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Design.color.white,
+    borderRadius: Design.borderRadius,
+    width: "100%",
+    paddingVertical: Design.space.large,
+    paddingHorizontal: Design.space.large,
+  },
+});
---
+++ src/namespaces/Design.ts
@@ -0,0 +1,20 @@
+export namespace Design {
+  export const fontSize = {
+    large: 24,
+    medium: 16,
+    small: 12,
+  };
+
+  export const color = {
+    white: "#FFFFFF",
+    beige: "#f2e9df",
+  };
+
+  export const space = {
+    large: 16,
+    medium: 8,
+    small: 4,
+  };
+
+  export const borderRadius = 15;
+}
