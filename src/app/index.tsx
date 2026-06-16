0d822b29d2ff965885fe3cd3ce79a06902049743
Angel <angelsol299@hotmail.com>
2026-06-17 00:00:47 +0200

    chore: added refresh and copy buttons/icons

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,6 +5,8 @@ import { ThemedView } from "@/components/themed-view";
 import { Card } from "@/components/ui/Card";
 import { BottomTabInset, MaxContentWidth, Spacing } from "@/constants/theme";
 import { Design } from "@/namespaces/Design";
+import { Ionicons } from "@expo/vector-icons";
+import Feather from "@expo/vector-icons/Feather";
 
 import {
   SplineSansMono_400Regular,
@@ -28,7 +30,14 @@ export default function HomeScreen() {
         <Card>
           <Text style={styles.password}>{"8213jhsj032190][pqwe"}</Text>
           <View style={styles.divider} />
-          <Text>hellow world</Text>
+          <View style={styles.iconsContainer}>
+            <View style={styles.iconContainer}>
+              <Ionicons name="refresh" size={18} color="black" />
+            </View>
+            <View style={styles.iconContainer}>
+              <Feather name="copy" size={18} color="black" />
+            </View>
+          </View>
         </Card>
       </SafeAreaView>
     </ThemedView>
@@ -68,5 +77,21 @@ const styles = StyleSheet.create({
   password: {
     fontSize: Design.fontSize.large,
     marginBottom: 42,
+  },
+
+  iconContainer: {
+    backgroundColor: "#f9f9f9",
+    padding: 12,
+    borderRadius: 16,
+    borderWidth: 0.5,
+    borderColor: "#e5e5e5",
+  },
+  iconsContainer: {
+    gap: Design.space.medium,
+    paddingTop: Design.space.large,
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-end",
   },
 });
