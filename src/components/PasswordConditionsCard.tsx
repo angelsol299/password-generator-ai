1a493814e838c605af4c4edc9237aeffbc897cc4
Angel <angelsol299@hotmail.com>
2026-06-18 16:07:55 +0200

    chore: made SwitchComponent reusable

--- src/components/PasswordConditionsCard.tsx
+++ src/components/PasswordConditionsCard.tsx
@@ -1,16 +1,17 @@
 import { Design } from "@/namespaces/Design";
 import { useState } from "react";
-import { StyleSheet, Switch, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { SwitchComponent } from "./SwitchComponent";
 import { Card } from "./ui/Card";
 
-interface ValueState {
+export interface ValueState {
   upperCase: boolean;
 }
 
 export const PasswordConditionsCard = () => {
   const [values, setValues] = useState<ValueState>({ upperCase: false });
 
-  const setValueFunc = (val: ConstrainBoolean) => {
+  const setValueFunc = (val: boolean) => {
     console.log({ val });
     setValues((prev) => ({ ...values, upperCase: !prev.upperCase }));
   };
@@ -18,17 +19,7 @@ export const PasswordConditionsCard = () => {
   return (
     <Card>
       <View style={styles.container}>
-        <View style={styles.upperCaseContainer}>
-          <View style={styles.textContainer}>
-            <Text style={styles.title}>Uppercase</Text>
-            <Text style={styles.subTitle}>A-Z</Text>
-          </View>
-          <Switch
-            value={values.upperCase}
-            onValueChange={setValueFunc}
-            trackColor={{ true: Design.color.blue }}
-          />
-        </View>
+        <SwitchComponent setValueFunc={setValueFunc} values={values} />
         <View style={styles.divider} />
         <View />
       </View>
@@ -38,24 +29,7 @@ export const PasswordConditionsCard = () => {
 
 const styles = StyleSheet.create({
   divider: { height: 1, backgroundColor: Design.color.lightGray },
-  upperCaseContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
   container: {
     gap: Design.space.medium,
-  },
-  title: {
-    fontFamily: Design.fontFamily.interSemiBold,
-    fontSize: Design.fontSize.medium,
-    color: Design.color.lightBrown,
-  },
-  subTitle: {
-    fontFamily: Design.fontFamily.splineRegular,
-    fontSize: Design.fontSize.small,
-    color: Design.color.lightBrown,
-  },
-  textContainer: {
-    gap: Design.space.xSmall,
   },
 });
---
+++ src/components/SwitchComponent.tsx
@@ -0,0 +1,48 @@
+import { Design } from "@/namespaces/Design";
+import { StyleSheet, Switch, Text, View } from "react-native";
+import { ValueState } from "./PasswordConditionsCard";
+
+interface SwitchComponentProps {
+  setValueFunc: (val: boolean) => void;
+  values: ValueState;
+}
+
+export const SwitchComponent = ({
+  setValueFunc,
+  values,
+}: SwitchComponentProps) => {
+  return (
+    <View style={styles.upperCaseContainer}>
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>Uppercase</Text>
+        <Text style={styles.subTitle}>A-Z</Text>
+      </View>
+      <Switch
+        value={values.upperCase}
+        onValueChange={setValueFunc}
+        trackColor={{ true: Design.color.blue }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  divider: { height: 1, backgroundColor: Design.color.lightGray },
+  upperCaseContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  title: {
+    fontFamily: Design.fontFamily.interSemiBold,
+    fontSize: Design.fontSize.medium,
+    color: Design.color.lightBrown,
+  },
+  subTitle: {
+    fontFamily: Design.fontFamily.splineRegular,
+    fontSize: Design.fontSize.small,
+    color: Design.color.lightBrown,
+  },
+  textContainer: {
+    gap: Design.space.xSmall,
+  },
+});
